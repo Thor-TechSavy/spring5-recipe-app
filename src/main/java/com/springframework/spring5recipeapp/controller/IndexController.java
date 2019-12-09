fd0dc980d6c5d192f6be47e704bdb2a8a70492e8
Shivam Ahuja <techsavythor@gmail.com>
2019-12-09 23:43:06 +0100

    added bootstrap v3.3.7

--- src/main/java/com/springframework/spring5recipeapp/controller/IndexController.java
+++ src/main/java/com/springframework/spring5recipeapp/controller/IndexController.java
@@ -1,9 +1,11 @@
 package com.springframework.spring5recipeapp.controller;
 import com.springframework.spring5recipeapp.services.RecipeService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@Slf4j
 @Controller
 public class IndexController {
 
@@ -15,7 +17,7 @@ public class IndexController {
 
     @RequestMapping({"", "/", "/index"})
     public String getIndexPage(Model model) {
-
+        log.debug("Getting Index Page");
         model.addAttribute("recipes", recipeService.getRecipes());
 
         return "index";
--- src/main/java/com/springframework/spring5recipeapp/services/impl/RecipeServiceImpl.java
+++ src/main/java/com/springframework/spring5recipeapp/services/impl/RecipeServiceImpl.java
@@ -3,11 +3,13 @@ package com.springframework.spring5recipeapp.services.impl;
 import com.springframework.spring5recipeapp.domain.Recipe;
 import com.springframework.spring5recipeapp.repositories.RecipeRepository;
 import com.springframework.spring5recipeapp.services.RecipeService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
 import java.util.Set;
 
+@Slf4j
 @Service
 public class RecipeServiceImpl implements RecipeService {
 
@@ -19,6 +21,7 @@ public class RecipeServiceImpl implements RecipeService {
 
     @Override
     public Set<Recipe> getRecipes() {
+        log.debug("I am in the service");
         Set<Recipe> recipeSet = new HashSet<>();
         recipeRepository.findAll().iterator().forEachRemaining(recipeSet::add);
         return recipeSet;
