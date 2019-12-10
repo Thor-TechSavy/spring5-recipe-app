8809554eae9bc39825ded8f700956a29db451b2b
Shivam Ahuja <techsavythor@gmail.com>
2019-12-10 23:46:42 +0100

    added index controller test

---
+++ src/test/java/com/springframework/spring5recipeapp/controller/IndexControllerTest.java
@@ -0,0 +1,40 @@
+package com.springframework.spring5recipeapp.controller;
+
+import com.springframework.spring5recipeapp.services.RecipeService;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.ui.Model;
+
+import static org.junit.Assert.*;
+import static org.mockito.ArgumentMatchers.anySet;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+public class IndexControllerTest {
+
+    @Mock
+    RecipeService recipeService;
+
+    @Mock
+    Model model;
+
+    IndexController indexController;
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+        indexController = new IndexController(recipeService);
+    }
+
+    @Test
+    public void getIndexPage() {
+        String viewName = indexController.getIndexPage(model);
+
+        assertEquals("index", viewName);
+        verify(recipeService,times(1)).getRecipes();
+        verify(model, times(1)).addAttribute(eq("recipes"), anySet());
+    }
+}
