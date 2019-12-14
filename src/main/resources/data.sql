ed8e192447ae12ece0b66dde031adece6a65230d
Shivam Ahuja <techsavythor@gmail.com>
2019-12-14 02:29:05 +0100

    added deploy key

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO category (description) VALUES ('American') ;
+INSERT INTO category (description) VALUES ('American');
 INSERT INTO category (description) VALUES ('Italian');
 INSERT INTO category (description) VALUES ('Mexican');
 INSERT INTO category (description) VALUES ('Fast Food');
